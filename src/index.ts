54651fc6abae244275e7f348bcf3dbd5f6192ec7
NoxFly <dorian.thivolle@gmail.com>
2026-03-11 15:18:03 +0100

    -

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@
 export * from './request';
 export * from './renderer-events';
 export * from './renderer-client';
+export type { HttpMethod, AtomicHttpMethod } from './decorators/method.decorator';
